f438fd4eff6a263c5b5f04bad0a1a89b58e115cc
okami <alisovski@mail.ru>
2020-02-07 00:39:11 +0300

    Выделен класс Report()

---
+++ tests/test_output_file.py
@@ -0,0 +1,11 @@
+import os
+
+def validate_output_file(file_name):
+    try:
+        file = open(file_name, "r")
+    except FileNotFoundError:
+        print(f'File {file_name} not found!')
+        return False
+    finally:
+        file.close()
+    return
